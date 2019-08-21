8b9984be3c06a35cf5866aad046e668c9617b4a4
caueguedes <caueguedes@univem.edu.br>
2019-08-21 19:15:06 -0300

    tarefa 3
    estilização de tabela

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,15 @@ require 'swapi'
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def home
     @movies = Swapi.fetchAll
-    # puts @movies["results"][1] 
   end
+
+  def ranking
+  end
+
 
   # def details
   #   @details = Swapi.fetch(params[:id])
