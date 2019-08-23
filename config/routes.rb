4a3bbfbe56ea0830630cd1b5b3c3b541f559ecab
caueguedes <caueguedes@univem.edu.br>
2019-08-23 16:03:59 -0300

    update routes.rb
    missed archive

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'core#home'
   get '/home', to: 'core#home' 
   get '/ranking', to: 'core#ranking', as: 'ranking'
-  get '/detail/:id', to: 'core#details', as: 'detail'
+  get '/detail/:id', to: 'core#detail', as: 'detail'
 
   resources :like, only: [:create, :destroy] 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
